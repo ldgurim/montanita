76ca646ca5ccde4d8317a12c69898ff5e462bd93
gurim <sergiogrimm@gmail.com>
2020-07-19 23:04:56 -0500

    gu_Movimiento y cámara básicos

---
+++ montanita/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+
+    
+{
+    public CharacterController controller;
+
+    public float speed = 5f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothvelocity;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothvelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
